d78359823226af9329e7a69debf7cb3721154869
Gerd Weitenberg <gweitenb@gmail.com>
2020-01-09 14:37:05 +0100

    phpspec for eu vat number validator and rates

---
+++ spec/Vat/Number/EuValidatorSpec.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Gweb\SyliusVATPlugin\Vat\Number;
+
+use Gweb\SyliusVATPlugin\Vat\Number\ClientException;
+use Gweb\SyliusVATPlugin\Vat\Number\ValidatorInterface;
+use Ibericode\Vat\Validator;
+use Ibericode\Vat\Vies\ViesException;
+use PhpSpec\ObjectBehavior;
+
+class EuValidatorSpec extends ObjectBehavior
+{
+    function let(Validator $validator)
+    {
+        $validator->validateVatNumberFormat('XY123')->willReturn(false);
+        $validator->validateVatNumberFormat('DE123123123')->willReturn(true);
+        $validator->validateVatNumber('DE123123123')->willReturn(true);
+        $validator->validateVatNumber('DE321321321')->willThrow(ViesException::class);
+
+        $this->beConstructedWith($validator);
+    }
+
+    function it_implements_vat_validator_interface(): void
+    {
+        $this->shouldImplement(ValidatorInterface::class);
+    }
+
+    function it_validate_vat_number_country()
+    {
+        $this->validateCountry('DE123123123', 'DE')->shouldReturn(true);
+        $this->validateCountry('DE123123123', 'de')->shouldReturn(true);
+        $this->validateCountry('DE123123123', 'FR')->shouldReturn(false);
+    }
+
+    function it_validate_vat_number_format()
+    {
+        $this->validateFormat('XY123')->shouldReturn(false);
+        $this->validateFormat('DE123123123')->shouldReturn(true);
+    }
+
+    function it_validate_vat_number()
+    {
+        $this->validate('DE123123123')->shouldReturn(true);
+    }
+
+    function it_should_throw_client_exception_if_validation_service_is_down()
+    {
+        $this->shouldThrow(ClientException::class)->during('validate', ['DE321321321']);
+    }
+}
---
+++ spec/Vat/Rates/EuRatesSpec.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Gweb\SyliusVATPlugin\Vat\Rates;
+
+use Gweb\SyliusVATPlugin\Vat\Rates\RatesInterface;
+use Ibericode\Vat\Rates;
+use PhpSpec\ObjectBehavior;
+
+class EuRatesSpec extends ObjectBehavior
+{
+    function let(Rates $rates)
+    {
+        $rates->getRateForCountry('DE', RatesInterface::RATE_STANDARD)->willReturn(0.19);
+        $rates->getRateForCountry('DE', RatesInterface::RATE_REDUCED)->willReturn(0.07);
+
+        $this->beConstructedWith($rates);
+    }
+
+    function it_implements_vat_rates_interface(): void
+    {
+        $this->shouldImplement(RatesInterface::class);
+    }
+
+    function it_should_return_eu_countries()
+    {
+        $this->getCountries()->shouldBeArray();
+        $this->getCountries()->shouldHaveKey('DE');
+        $this->getCountries()->shouldHaveKey('FR');
+        $this->getCountries()->shouldHaveKey('NL');
+
+        $this->getCountries()->shouldNotHaveKey('US');
+    }
+
+    function it_should_return_eu_country_vat_rate()
+    {
+        $this->getCountryRate('DE', RatesInterface::RATE_STANDARD)->shouldReturn(0.19);
+        $this->getCountryRate('DE', RatesInterface::RATE_REDUCED)->shouldReturn(0.07);
+    }
+}
